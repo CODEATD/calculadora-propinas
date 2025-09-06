6d841f626afc2f9c3d55911e25c0a286c25fb2f7
CODE ATD <codeatd@gmail.com>
2025-09-06 00:26:34 -0400

    110. Agregando los Contenedores Principales

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import Header from "./components/Header";
 import { menuItems } from "./data/db";
 
 function App() {
-
   console.log(menuItems);
   return (
     <>
-        <Header />
+      <Header />
+
+      <main className=" max-w-7xl w-[90%] mx-auto py-20 grid grid-cols-1 md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
