324c18331f745079df4fef715868d88da3c6bd55
CODE ATD <codeatd@gmail.com>
2025-12-13 00:01:50 -0400

    123. Primeros pasos con las propinas

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import MenuItem from "./components/MenuItem";
 import OrderContents from "./components/OrderContents";
 import OrderTotals from "./components/OrderTotals";
+import TipPercentageForm from "./components/TipPercentageForm";
 import { menuItems } from "./data/db";
 import useOrder from "./hooks/useOrder";
 
@@ -24,9 +25,8 @@ function App() {
 
         <div className=" border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
           <OrderContents order={order} removeItem={removeItem} />
-          <OrderTotals
-            order={order}
-          />
+          <TipPercentageForm />
+          <OrderTotals order={order} />
         </div>
       </main>
     </>
