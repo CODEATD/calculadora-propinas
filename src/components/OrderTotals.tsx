62c71d0c65fbe369b82b6f731109d06e6de152d9
CODE ATD <codeatd@gmail.com>
2025-12-22 22:23:43 -0400

    Lección: 128. Reiniciando la Orden

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -3,11 +3,12 @@ import { formatCurrency } from "../helpers";
 import type { OrderItem } from "../types";
 
 type OrderTotalsProps = {
-  order: OrderItem[];
-  tip: number;
+  order: OrderItem[],
+  tip: number,
+  placeOrder: () => void,
 };
 
-export default function OrderTotals({ order, tip }: OrderTotalsProps) {
+export default function OrderTotals({ order, tip, placeOrder }: OrderTotalsProps) {
   const subtotalAmount = useCallback(
     () => order.reduce((total, item) => total + item.quantity * item.price, 0),
     [order]
@@ -35,7 +36,13 @@ export default function OrderTotals({ order, tip }: OrderTotalsProps) {
         </p>
       </div>
 
-      <button></button>
+      <button
+        className="w-full bg-black p-3 uppercase text-white font-bold mt-10 disabled:opacity-10"
+        disabled={totalAmount() === 0}
+        onClick={placeOrder}
+      >
+        Guardar Orden
+      </button>
     </>
   );
 }
