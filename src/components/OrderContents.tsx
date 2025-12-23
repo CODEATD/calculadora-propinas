560c3bdbda52feaec3e94ee33352105d85dfed42
CODE ATD <codeatd@gmail.com>
2025-12-22 22:23:43 -0400

    Lección: 128. Reiniciando la Orden

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -15,10 +15,7 @@ export default function OrderContents({
       <h2 className=" font-black text-4xl">Consumo</h2>
 
       <div className="space-y-3 mt-10">
-        {order.length === 0 ? (
-          <p className=" text-center">La orden está vacía</p>
-        ) : (
-          order.map((item) => (
+        {order.map((item) => (
             <div
               key={item.id}
               className=" flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b"
@@ -39,8 +36,7 @@ export default function OrderContents({
                 X
               </button>
             </div>
-          ))
-        )}
+          ))}
       </div>
     </div>
   );
